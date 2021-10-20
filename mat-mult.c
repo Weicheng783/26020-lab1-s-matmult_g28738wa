b2d17f6f62373e4a5328544dc90fc16dae29750e
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2021-10-20 15:07:03 +0100

    Finish Work, Final Check Completed.

--- mat-mult.c
+++ mat-mult.c
@@ -2,39 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-// int gettimeofday();
-// int timersub();
-int isdigit();
 int main(int argc, char **argv){
+	/* We declare variables of start and finish time, duration,
+	   And three arrays by using 2-dimensional arrays.
+	*/
 	clock_t start, finish;
 	double duration;
 	int **matrix_array1;
 	int **matrix_array2;
 	int **matrix_array_result;
-	// struct timeval start, stop, elapsed;
-	// gettimeofday(&start, NULL);
 
-	// Check that we have the right number of arguments
+	// Check that we have the right number of arguments.
 	if(argc != 4){
 		printf("ERROR: incorrect number of arguments, please read the usage below.\n");
 		printf("Usage: %s <N> <M> <seed>\n", argv[0]);
 		return -1;
-	}
-	//HAS PROBLEM...
-	// else if(atoi(argv[1]) == 0 || isdigit(atoi(argv[2])) == 0 || isdigit(atoi(argv[3])) == 0){
-	// 	printf("ERROR: invalid arguments.\n");
-	// 	printf("Usage: %s <N> <M> <seed>\n", argv[0]);
-	// 	return -1;
-	// }
-	else{
-		// All arguments should be converted to integers
+	}else{
+		// All arguments should be converted to integers.
 		int M = atoi(argv[1]);
-		// printf("%d\n", M);
 		int N = atoi(argv[2]);
+		// We fetch the seed number from the argv[3], which is the user's fourth input.
 		int seed = atoi(argv[3]);
-		srand((unsigned) seed); //time( NULL )
+		srand((unsigned) seed);
 
-		
+		// Here we check each matrix dimension number entered.
+		if(M == 0 || N == 0){
+			printf("ERROR: invalid arguments. Please read the usage below.\n");
+			printf("Usage: %s <N> <M> <seed>\n", argv[0]);
+			return -1;
+		}
+
+		// We use malloc to dynamically allocate memories for these arrays.
+		// And we need to check if each array returns null.
 		matrix_array1 = malloc(M * sizeof(int *));
 		if(matrix_array1 == NULL) return -1;
 		matrix_array2 = malloc(N * sizeof(int *));
@@ -42,6 +41,8 @@ int main(int argc, char **argv){
 		matrix_array_result = malloc(M * sizeof(int *));
 		if(matrix_array_result == NULL) return -1;
 
+		// Hence we allocate memory to each of the numbers in each of matrices.
+		// Including the resulted matrix.
 		for(int i = 0; i<M; i++){
 			matrix_array1[i] = malloc(N * sizeof(int));
 			if (matrix_array1[i] == NULL) return -1;
@@ -57,6 +58,7 @@ int main(int argc, char **argv){
 			if (matrix_array_result[i] == NULL) return -1;
 		}
 
+		// We print the formatted result to the console while we assign values.
 		printf("MATRIX 1:\n");
 		for(int i = 0; i<M; i++){
 			for(int j = 0; j<N; j++){
@@ -69,7 +71,6 @@ int main(int argc, char **argv){
 			}
 			printf("\n");
 		}
-
 
 		printf("MATRIX 2:\n");
 		for(int i = 0; i<N; i++){
@@ -84,18 +85,24 @@ int main(int argc, char **argv){
 			printf("\n");
 		}
 
+		// We first initialise the values in result matrix to 0.
+		for(int i = 0; i<M; i++){
+			for(int j = 0; j<M; j++){
+				matrix_array_result[i][j] = 0;
+			}
+		}
 
 		printf("RESULT:\n");
+		// Start the clock when the result part executes.
 		start = clock();
-	    // Iterate over rows of mat1
-	    // printf("%d\n", sizeof(matrix_array1)/sizeof(matrix_array1[0])/sizeof(matrix_array1[0][0]));
-	    // printf("%d\n", sizeof(matrix_array1[0]));
+	    // Iterate over rows of mat1.
 	    for(int i = 0; i < M; i++){
-	        // Iterate over columns of mat2:
+	        // Iterate over columns of mat2.
 	        for(int j = 0; j < M; j++){
-	            // Iterate over rows of mat2:
+	            // Iterate over rows of mat2.
 	            for(int k = 0; k < N; k++){
-	                matrix_array_result[i][j] = matrix_array_result[i][j] + matrix_array1[i][k] * matrix_array2[k][j];
+	            	// Logic of matrix multiplication.
+	                matrix_array_result[i][j] = matrix_array_result[i][j] + (matrix_array1[i][k] * matrix_array2[k][j]);
 	            }
                 if(j == M-1){
             		printf("%d\n",matrix_array_result[i][j]);
@@ -104,25 +111,21 @@ int main(int argc, char **argv){
                 }
 	        }
 	    }
-		// printf("%d\n", rand()%100);
-		// mat1 = [[random.randrange(100) for i in range(M)] for j in range(N)];
-		// for(int i=0; i<1000000; i++){
-		// 	if(rand()%100 == 100){
-		// 		printf("ERROR! MEET 100!\n");
-		// 		exit(-1);
-		// 	}
-		// }
-		// printf("Done LOOP.\n");
+	    // Close the clock to finish the timing requirements.
 		finish = clock();
 	}
-	// gettimeofday(&stop,NULL);
-	// timersub(&stop, &start, &elapsed);
 	
+	// Time conversion.
 	duration = (double) (finish - start) / CLOCKS_PER_SEC;
-	// printf("EXECUTION TIME: %lu.%06lu\n", elapsed.tv_sec, elapsed.tv_usec); //%f
+
+	// We need to free every matrix's memory after use.
 	free(matrix_array1);
 	free(matrix_array2);
 	free(matrix_array_result);
+
+	// Finally display the execution time. 
 	printf("EXECUTION TIME: %f\n", duration);
+
+	// And return the code 0 for exit normally.
 	return 0;
 }
